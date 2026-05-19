298236d870fd7146e99c64d1e33cf2fddff10ff5
kiennguyen <kiennguyen.08122004@gmail.com>
2026-05-19 10:39:17 +0700

    kien creat component helloName 2

---
+++ src/components/HelloNAame.jsx
@@ -0,0 +1,5 @@
+function HelloNAame() {
+  const name = "trung kien";
+  return <>hello {name}</>;
+}
+export default HelloNAame;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 //import "./index.css";
 //import App from "./App.jsx";
 import Hello from "./components/Hello";
+import HelloNAame from "./components/HelloNAame";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Hello />
+    <HelloNAame />
   </StrictMode>,
 );
